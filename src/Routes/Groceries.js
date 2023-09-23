e58a5c358e79b9aea82fbdc19a70a374007bd5f2
hassan <hassanajibade17@gmail.com>
2023-09-23 18:55:09 +0100

    cookie commit

--- src/Routes/Groceries.js
+++ src/Routes/Groceries.js
@@ -25,11 +25,15 @@ const groceryList = [
 
 
 router.get('/', (req,res)=>{
+    res.cookie('visited', true, {
+        maxAge: 60000
+    })
     res.send(groceryList)
     
 })
 
 router.get('/:item',(req,res)=>{
+    console.log(req.cookies)
     const {item} = req.params;
     const groceryItem = groceryList.find(g => g.item === item)
     res.send(groceryItem);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const groceriesStoreRoutes = require('./Routes/Groceries')
 const marketRoutes = require('./Routes/Markets')
+const cookieParser = require('cookie-parser')
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser())
 app.use('/api/groceries',groceriesStoreRoutes)
 app.use('/api/markets', marketRoutes)
 app.use((req,res,next) =>{
