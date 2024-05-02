ee0ea229e76fd0ad6bffec9d1c471e2fa482eb6d
hassan <hassanajibade17@gmail.com>
2024-05-02 18:32:14 +0100

    session part 1

--- Routes/Products.mjs
+++ Routes/Products.mjs
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import session from "express-session";
 
 const router = Router();
 
 router.get('/api/products', (req, res)=> {
-    console.log(req.headers.cookie)
+    console.log(req.session)
+    console.log(req.session.id)
     console.log(req.cookies)
     res.status(200).send([{name: 'Milk', category: 'Grocery', price: 200, amount: 10}])
 })
--- src/index.mjs
+++ src/index.mjs
@@ -5,10 +5,21 @@ import userRouter from '../Routes/Users.mjs'
 import { mockUsers } from "./utils/Constant.mjs";
 import { loggingMiddleware } from "./utils/Middlewares.mjs";
 import productRouter from '../Routes/Products.mjs'
+import session from "express-session";
+
+
 const app = express()
 
 app.use(json())
 app.use(cookieParser())
+app.use(session({
+    secret: 'hassan the dev',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 60000 * 60
+    }
+}))
 app.use(productRouter)
 app.use(userRouter)
 app.use(loggingMiddleware)
@@ -21,6 +32,8 @@ const PORT = process.env.PORT || 3001;
 
 app.get("/",
        (req, res)=>{
+        req.session.visited= true
+        console.log(req.session, req.session.id)
         res.cookie('hello', 'world', { maxAge: 60000 * 60 * 2 })
         const result = validationResult(req)
         if(!result.isEmpty()) return res.send({ error: result.array()}).status(400)
